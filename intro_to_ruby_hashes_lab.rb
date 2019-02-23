794ed1b013cebdaeaa52a634fbe95656f2eb0589
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-23 21:03:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,7 +51,7 @@ def monopoly_with_fourth_tier
 				:reading_railroad => {"mortgage_value" => "$100"},
 				:pennsylvania_railroad => {"mortgage_value" => '$200'},
 				:b_and_o_railroad => {"mortgage_value" => '$400'},
-				:shortline => {}
+				:shortline => {"mortgage_value" => '$800'}
 				},
 				:rent_in_dollars =>
 		{
