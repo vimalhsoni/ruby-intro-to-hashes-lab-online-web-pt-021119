898dd6c267ce6223d226eb1a239526b61ca59ecc
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-23 20:23:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,6 @@ def monopoly_with_third_tier
 	    :names=>
 	    {}
 	  }
-	}
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
